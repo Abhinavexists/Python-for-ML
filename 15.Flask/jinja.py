25344cc10c3fc9f8e6ec837039a28162819e3e9a
Abhinavks1405 <abhinavkumarsingh2023@gmail.com>
2024-10-24 23:54:27 +0530

    dynamic url

--- 15.Flask/jinja.py
+++ 15.Flask/jinja.py
@@ -2,7 +2,7 @@
 # variable rule
 # Jinja 2 Template Engine
 
-from flask import Flask , render_template ,request
+from flask import Flask , render_template ,request , redirect , url_for
 '''
  It creates an instance of the Flask class, 
  which will be your WSGI (Web Server Gateway Interface) application.
@@ -32,14 +32,14 @@ def form():
         return f"Hello {name} , email {mail} , my message is {message}!"
     return render_template('form.html')
 
-@app.route('/submit' , methods = ['GET' , 'POST'])
-def submit():
-    if request.method == 'POST':
-        name = request.form['name']
-        mail = request.form['email']
-        message = request.form['message']
-        return f"Hello {name} , email {mail} , my message is {message}!"
-    return render_template('form.html')
+# @app.route('/submit' , methods = ['GET' , 'POST'])
+# def submit():
+#     if request.method == 'POST':
+#         name = request.form['name']
+#         mail = request.form['email']
+#         message = request.form['message']
+#         return f"Hello {name} , email {mail} , my message is {message}!"
+#     return render_template('form.html')
 
 #variable rule
 @app.route('/success/<int:score>') #<> inside this is the parameter
@@ -70,6 +70,26 @@ def successres(score):
 @app.route('/successif/<int:score>') 
 def successif(score):
      return render_template('result1.html' , results = score)
+
+@app.route('/fail/<int:score>')
+def fail(score):
+
+    return render_template('results.html',results = score)
+
+@app.route('/submit' , methods = ['POST' , 'GET'])
+def submit():
+    total_score = 0
+    if request.method == 'POST':
+        science=float(request.form['science'])
+        maths=float(request.form['maths'])
+        c=float(request.form['c'])
+        data_science=float(request.form['datascience'])
+
+        total_score=(science+maths+c+data_science)/4
+    else:
+        return render_template('getres.html')
+    return redirect(url_for('successres',score=total_score)) # redirecte to successres
+
 
 if __name__ == "__main__":
     app.run(debug = True)
